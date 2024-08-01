3dcd37e9f5369eab59582cfce9eb3dbc1596c85d
Ajay Anand <writetoajayanand@gmail.com>
2024-08-01 16:36:45 -0600

    add display access and additional permissions

--- run.bash
+++ run.bash
@@ -1,1 +1,3 @@
-docker run -it osrf/ros:humble-desktop
+xhost +
+docker run -it --privileged --name ros_humble_testing  --net=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix osrf/ros:humble-desktop
+xhost-
