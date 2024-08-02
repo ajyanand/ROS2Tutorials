53267b849cb9f6953b80568cbec8c3a4cc8157cc
Ajay Anand <writetoajayanand@gmail.com>
2024-08-02 15:57:24 -0600

    typo bug fix

--- RunDockerfile.bash
+++ RunDockerfile.bash
@@ -1,4 +1,4 @@
 xhost +
 docker container prune 
 docker run -it --privileged --name ros_humble_testing  --net=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix ajyanand/ros2gazebo_test:latest
-xhost-
+xhost -
