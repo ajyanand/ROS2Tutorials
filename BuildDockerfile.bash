ceffcd8cd22fdeb9565c5f909061f158a3edcb8e
Ajay Anand <writetoajayanand@gmail.com>
2024-08-02 13:25:57 -0600

    errorfix

--- BuildDockerfile.bash
+++ BuildDockerfile.bash
@@ -1,4 +1,4 @@
-docker build ./ --pull --no-cache -t ajyanand/ROS2GazeboTest:latest
+docker build ./ --pull --no-cache -t ajyanand/ros2gazebo_test:latest
 # --pull always attempts to pull a newer version of the base image
 # --no-cache forces a rebuild of the image
 # -t tag the image with the name ajyanand/ROS2GazeboTest:latest
